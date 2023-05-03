69638398039355a979b522167f3347e7e368e8fa
Tomas_Fonseca <tomas.g.fonseca@gmail.com>
2023-05-03 23:26:51 +0100

    Design and other changes

--- script.js
+++ script.js
@@ -1,54 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Apply animations to elements
+  const fadeInUpElements = document.querySelectorAll(".fadeInUp");
+  const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+          if (entry.intersectionRatio > 0) {
+              entry.target.classList.add("animated");
+          }
+      });
+  });
 
-document.addEventListener('DOMContentLoaded', function () {
-  const funButton = document.querySelector('.btn-fun');
-
-  if (funButton) {
-    funButton.addEventListener('click', function (event) {
-      event.preventDefault();
-      smoothScroll('#contacts');
-    });
-  }
-
-// Add animation to navbar links when clicked
-const navLinks = document.querySelectorAll('.nav-link');
-navLinks.forEach(link => {
-  link.addEventListener('click', () => {
-    link.classList.add('animate__animated', 'animate__swing');
-    setTimeout(() => {
-      link.classList.remove('animate__animated', 'animate__swing');
-    }, 1000);
+  fadeInUpElements.forEach(el => {
+      observer.observe(el);
   });
 });
 
-// Add animation to hero section text
-const heroTitle = document.querySelector('.display-4');
-heroTitle.addEventListener('mouseenter', () => {
-  heroTitle.classList.add('animate__animated', 'animate__heartBeat');
-});
-heroTitle.addEventListener('animationend', () => {
-  heroTitle.classList.remove('animate__animated', 'animate__heartBeat');
-});
+document.addEventListener('DOMContentLoaded', function() {
+  var navButton = document.querySelector('.navbar-toggler');
+  var navCollapse = document.querySelector('#navbarNav');
 
-// Add animation to education and experience sections
-const sectionTitles = document.querySelectorAll('#education h3, #experience h3');
-sectionTitles.forEach(title => {
-  title.addEventListener('mouseenter', () => {
-    title.classList.add('animate__animated', 'animate__wobble');
+  navButton.addEventListener('click', function() {
+      navCollapse.classList.toggle('collapse');
   });
-  title.addEventListener('animationend', () => {
-    title.classList.remove('animate__animated', 'animate__wobble');
-  });
-});
-
-// Add animation to skills section icons
-const skillIcons = document.querySelectorAll('.col-md-4 i');
-skillIcons.forEach(icon => {
-  icon.addEventListener('mouseenter', () => {
-    icon.classList.add('animate__animated', 'animate__jello');
-  });
-  icon.addEventListener('animationend', () => {
-    icon.classList.remove('animate__animated', 'animate__jello');
-  });
-});
-
 });
